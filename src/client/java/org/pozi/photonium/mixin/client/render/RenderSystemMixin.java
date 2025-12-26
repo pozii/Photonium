77fd71b84ef466b63f2495f683990c504f3ae0c1
Pozi <pozi@logicbyte.org>
2025-12-26 22:39:17 +0300

    Update RenderSystemMixin.java

--- src/client/java/org/pozi/photonium/mixin/client/render/RenderSystemMixin.java
+++ src/client/java/org/pozi/photonium/mixin/client/render/RenderSystemMixin.java
@@ -8,11 +8,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderSystem.class)
 public class RenderSystemMixin {
-    /**
-     * Oyun her karede "Render Thread'inde miyim?" diye kontrol eder.
-     * Bu mixin, kontrol başlar başlamaz "Evet öylesin" diyerek işlemi iptal eder
-     * ve işlemciyi boş yere yormaz.
-     */
     @Inject(method = "assertOnRenderThread", at = @At("HEAD"), cancellable = true)
     private static void onAssertOnRenderThread(CallbackInfo ci) {
         ci.cancel();
