5b95693e58678c7312fe17ab2c0d091916bf4785
Pozi <pozi@logicbyte.org>
2025-12-26 22:39:07 +0300

    Update EntityRendererMixin.java

--- src/client/java/org/pozi/photonium/mixin/client/render/EntityRendererMixin.java
+++ src/client/java/org/pozi/photonium/mixin/client/render/EntityRendererMixin.java
@@ -16,16 +16,11 @@ public abstract class EntityRendererMixin<T extends Entity> {
     @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
     public void shouldRender(T entity, Frustum frustum, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
         if (MinecraftClient.getInstance().getCameraEntity() != null) {
-            // Varsayılan menzil: 64 Blok (Karesi 4096)
             double cutoffRange = 4096.0D;
-
-            // EĞER varlık yerdeki bir eşya (Item) veya XP puanı ise menzili kıs:
-            // 32 Blok (Karesi 1024)
             if (entity instanceof ItemEntity || entity instanceof ExperienceOrbEntity) {
                 cutoffRange = 1024.0D;
             }
 
-            // Mesafe kontrolü
             if (entity.squaredDistanceTo(MinecraftClient.getInstance().getCameraEntity()) > cutoffRange) {
                 cir.setReturnValue(false);
             }
