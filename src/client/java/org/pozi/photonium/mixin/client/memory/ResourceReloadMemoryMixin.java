dc573839b19ed31cb77f6421449e7f3771e9d338
Pozi <pozi@logicbyte.org>
2025-12-26 22:38:01 +0300

    Update ResourceReloadMemoryMixin.java

--- src/client/java/org/pozi/photonium/mixin/client/memory/ResourceReloadMemoryMixin.java
+++ src/client/java/org/pozi/photonium/mixin/client/memory/ResourceReloadMemoryMixin.java
@@ -8,11 +8,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class ResourceReloadMemoryMixin {
-    /**
-     * Kaynaklar (Doku paketleri, Dil vb.) her değiştiğinde veya F3+T yapıldığında,
-     * WorldRenderer sınıfı kendini yenilemek için "reload" metodunu çalıştırır.
-     * Bu işlem bittiğinde (RETURN), RAM'deki çöpleri temizliyoruz (GC).
-     */
     @Inject(method = "reload()V", at = @At("RETURN"))
     private void onReload(CallbackInfo ci) {
         System.gc();
